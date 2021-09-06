12197d5eae7a7cbcfb60dba508da440e41870201
Brandon Santander <brandon.santander.terceros@gmail.com>
2021-09-05 22:26:09 -0400

    adding drone trip manager and remove unnecessary model

---
+++ Drones/Drones/Managers/SquadManager.cs
@@ -0,0 +1,63 @@
+﻿using Drones.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Drones.Managers
+{
+    /// <summary>
+    /// Manages the squad trip availability
+    /// </summary>
+    public class SquadManager
+    {
+        public List<List<DroneTrip>> SquadTrips { get; set; }
+
+        public SquadManager()
+        {
+            this.SquadTrips = new List<List<DroneTrip>>();
+        }
+
+        /// <summary>
+        /// Generates a list of list of drone trips with only drones info
+        /// </summary>
+        /// <param name="drones">drones available</param>
+        /// <param name="maxTrips">max possible trips</param>
+        public void GenerateMaxSquadTrips(List<Drone> drones, int maxTrips)
+        {
+            for (int i = 0; i < maxTrips; i++)
+            {
+                var droneTrips = drones
+                    .OrderByDescending(d => d.MaxWeight)
+                    .Select(d => new DroneTrip(d))
+                    .ToList();
+                this.SquadTrips.Add(droneTrips);
+            }
+        }
+
+        /// <summary>
+        /// Assign location to next available drone trip
+        /// </summary>
+        /// <param name="location"></param>
+        public void AssignLocationToNextAvailableDroneTrip(Location location)
+        {
+            bool assigned = false;
+
+            foreach (var droneTrips in this.SquadTrips)
+            {
+                foreach (var currentDroneTrip in droneTrips)
+                {
+                    if (currentDroneTrip.GetRemainingSpace() >= location.Weight)
+                    {
+                        currentDroneTrip.AddLocation(location);
+                        assigned = true;
+                        break;
+                    }
+                }
+                if (assigned)
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Drones/Drones/Models/DroneTrip.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Drones.Models
+{
+    /// <summary>
+    /// Drone Trip Class, it helps to keep track of trips by drone
+    /// </summary>
+    public class DroneTrip
+    {
+        private Drone _drone;
+        private List<Location> _locations;
+
+        public DroneTrip()
+        {
+            _locations = new List<Location>();
+        }
+
+        public DroneTrip (Drone drone)
+        {
+            _drone = drone;
+            _locations = new List<Location>();
+        }
+
+        /// <summary>
+        /// Adds a location to the list of locations, validates if there is available space
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public bool AddLocation(Location location)
+        {
+            if (location.Weight <= this.GetRemainingSpace())
+            {
+                _locations.Add(location);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Return total weight of locations
+        /// </summary>
+        /// <returns></returns>
+        public double GetTotalTripWeight()
+        {
+            double totalWeight = 0;
+            foreach (Location currentLocation in _locations)
+            {
+                totalWeight += currentLocation.Weight;
+            }
+
+            return totalWeight;
+        }
+
+        /// <summary>
+        /// Returns remaining drone available space based on the locations weights
+        /// </summary>
+        /// <returns></returns>
+        public double GetRemainingSpace()
+        {
+            return _drone.MaxWeight - this.GetTotalTripWeight();
+        }
+    }
+}
