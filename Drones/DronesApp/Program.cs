7374c718c8fc7bd6aede5be45a3f00035a3df3cc
Brandon Santander <brandon.santander.terceros@gmail.com>
2021-09-05 20:39:56 -0400

    adding base solution and projects

---
+++ Drones/DronesApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DronesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
